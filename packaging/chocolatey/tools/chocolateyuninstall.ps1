904822462160064cae1faa14393d10255330ba9f
jbrinkman <joe.brinkman@improving.com>
2025-08-04 11:56:01 -0400

    fix: Add whitespace for better readability in chocolateyuninstall.ps1

--- packaging/chocolatey/tools/chocolateyuninstall.ps1
+++ packaging/chocolatey/tools/chocolateyuninstall.ps1
@@ -4,7 +4,7 @@ $packageName = 'dotnetapidiff'
 
 # Remove extracted files
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Get-ChildItem -Path $toolsDir -Include *.exe,*.dll,*.json,*.xml,*.pdb -File | ForEach-Object { Remove-Item $_.FullName -Force }
+Get-ChildItem -Path $toolsDir -Include *.exe, *.dll, *.json, *.xml, *.pdb -File | ForEach-Object { Remove-Item $_.FullName -Force }
 
 # Clean up batch file if it exists
 $batchPath = Join-Path $toolsDir "dotnetapidiff.bat"
